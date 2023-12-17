8f8f14db26c9b9ff65f49e0b7ca0ae21b3d61888
Akshata <154059280+simple-logical-programs@users.noreply.github.com>
2023-12-17 04:30:32 -0800

    Add files via upload

---
+++ studentcurdOperation/src/main/java/com/studentcurd/StudentcurdOperationApplication.java
@@ -0,0 +1,13 @@
+package com.studentcurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentcurdOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentcurdOperationApplication.class, args);
+	}
+
+}
---
+++ studentcurdOperation/src/test/java/com/studentcurd/StudentcurdOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentcurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentcurdOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
